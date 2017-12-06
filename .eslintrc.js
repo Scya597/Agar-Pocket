d98e2150054e7de284414818b6a0cc42374b8d33
Scya597 <scya597@gmail.com>
2017-12-06 22:42:46 +0800

    :fire: add babel/eslint/gitignore file

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "parser": 'babel-eslint',
+    "parserOptions": {
+      "ecmaFeatures": {
+        "experimentalObjectRestSpread": true,
+        "jsx": true
+      },
+      "sourceType": "module"
+    },
+    "env": {
+      "browser": true,
+      "commonjs": true,
+      "es6": true,
+      "node": true
+    },
+    "rules": {
+      "no-console": 0,
+      "no-else-return": 0,
+      "global-require": 0,
+      "react/prop-types": 0,
+      "react/no-string-refs": 0,
+      "import/no-extraneous-dependencies": 0,
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    }
+}
