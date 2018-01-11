5bb2ec9fbfb779cbfaee8cdf80b055e0c6217ca1
Scya597 <scya597@gmail.com>
2018-01-12 00:49:56 +0800

    :fire: update physicsengine

--- server/physicsEngine.js
+++ server/physicsEngine.js
@@ -2,74 +2,88 @@ import uuid from 'uuid/v1';
 
 import Food from './entity/food';
 
-const countTwoCellDistance = (cellA, cellB, dt) => Math.sqrt((((cellA.pos.x + (cellA.vel.x * dt)) -
-  (cellB.pos.x)) ** 2) + (((cellA.pos.y + (cellA.vel.y * dt)) -
-  (cellB.pos.y)) ** 2));
+const countTwoCellDistance = (cellA, cellB, dtx, dty) =>
+  Math.sqrt((((cellA.pos.x + (cellA.vel.x * dtx)) - (cellB.pos.x)) ** 2)
+  + (((cellA.pos.y + (cellA.vel.y * dty)) - (cellB.pos.y)) ** 2));
 
-const checkTwoCellWillTouched = (cellA, cellB, dt) => {
-  if (countTwoCellDistance(cellA, cellB, dt) < (cellA.getRadius() + cellB.getRadius())) {
+const checkTwoCellWillTouched = (cellA, cellB, dtx, dty) => {
+  if (countTwoCellDistance(cellA, cellB, dtx, dty) < (cellA.getRadius() + cellB.getRadius())) {
     return true;
   } else {
     return false;
   }
 };
 
-const checkCellTouchBorderAndMove = (cell, setting, dt) => {
+const checkCellTouchBorderAndMove = (cell, setting, dtx, dty) => {
   const cellRadius = cell.getRadius();
   // if (dt > 1 / (60 * 32)) {
   //
   // }
-  if ((cell.pos.x + (cell.vel.x * dt)) - cellRadius >= 0 &&
-    (cell.pos.x + (cell.vel.x * dt)) + cellRadius <= setting.worldWidth) {
-    cell.pos.x += cell.vel.x * dt;
+  if ((cell.pos.x + (cell.vel.x * dtx)) - cellRadius >= 0 &&
+    (cell.pos.x + (cell.vel.x * dtx)) + cellRadius <= setting.worldWidth) {
+    cell.pos.x += cell.vel.x * dtx;
   }
-  if ((cell.pos.y + (cell.vel.y * dt)) - cellRadius >= 0 &&
-    (cell.pos.y + (cell.vel.y * dt)) + cellRadius <= setting.worldHeight) {
-    cell.pos.y += cell.vel.y * dt;
+  if ((cell.pos.y + (cell.vel.y * dty)) - cellRadius >= 0 &&
+    (cell.pos.y + (cell.vel.y * dty)) + cellRadius <= setting.worldHeight) {
+    cell.pos.y += cell.vel.y * dty;
   }
 };
 
 const updatePlayerPosition = (playerList, setting) => {
+  // 噴射碰撞
+  // 吃東西變大碰撞
   playerList.forEach((player) => {
     player.cellList.forEach((cell) => {
       // need to consider mass to vel
-      cell.vel.x = player.mousePos.x - cell.pos.x;
-      cell.vel.y = player.mousePos.y - cell.pos.y;
+      cell.vel.x = (player.mousePos.x - cell.pos.x) * (1000 / cell.mass);
+      cell.vel.y = (player.mousePos.y - cell.pos.y) * (1000 / cell.mass);
     });
 
-    const dt = 1 / 60;
+    const dtx = 1 / 60;
+    const dty = 1 / 60;
 
     if (player.checkSplit) {
       for (let i = 0; i < player.cellList.length; i += 1) {
         // check split case
         let checkTime = 0;
-        let timeRatio = 1;
+        let timeRatioX = 1;
+        let timeRatioY = 1;
         let willOverlap = false;
         while (checkTime < 5) {
           for (let j = 0; j < player.cellList.length; j += 1) {
             if (j !== i) {
               willOverlap = willOverlap ||
-                checkTwoCellWillTouched(player.cellList[i], player.cellList[j], dt * timeRatio);
+                checkTwoCellWillTouched(
+                  player.cellList[i], player.cellList[j],
+                  (dtx * timeRatioX), (dty * timeRatioY),
+                );
             }
           }
           if (willOverlap === true) {
-            timeRatio *= (1 / 2);
-            checkTime += 1;
+            if (timeRatioX === timeRatioY) {
+              timeRatioX *= (1 / 2);
+            } else {
+              timeRatioY *= (1 / 2);
+            }
+            checkTime += (1 / 2);
           } else {
             checkTime = 5;
           }
         }
 
         if (willOverlap === false) {
-          checkCellTouchBorderAndMove(player.cellList[i], setting, dt * timeRatio);
+          checkCellTouchBorderAndMove(
+            player.cellList[i], setting,
+            (dtx * timeRatioX), (dty * timeRatioY),
+          );
         } else {
-          console.log('jaaaa');
+          console.log('No overlap');
         }
       }
     } else {
       // check Border Case
       player.cellList.forEach((cell) => {
-        checkCellTouchBorderAndMove(cell, setting, dt);
+        checkCellTouchBorderAndMove(cell, setting, dtx, dty);
       });
     }
   });
